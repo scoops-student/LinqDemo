f6e1d401f7f6c64fe3c23f66e5683cfb50a41cc8
Hannah Kiekens <hannah.kiekens@scoops.com>
2020-09-25 11:16:15 +0200

    Write introduction

---
+++ Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqDemo
+{
+    public class Person
+    {
+        public enum Type
+        {
+            Unknown,
+            Student,
+            Employee,
+            Owner,
+        }
+
+        public int Id { get; set; } = 0;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public Person.Type PersonType { get; set; } = Type.Unknown;
+
+        public static IEnumerable<Person> Scoops()
+        {
+            return new List<Person>
+            {
+                new Person
+                {
+                    Id = 1,
+                    FirstName = "Nico",
+                    LastName = "Deleu",
+                    PersonType = Type.Owner,
+                },
+                new Person
+                {
+                    Id = 2,
+                    FirstName = "Hannah",
+                    LastName = "Kiekens",
+                    PersonType = Type.Employee,
+                },
+                new Person
+                {
+                    Id = 3,
+                    FirstName = "Ayoub",
+                    LastName = "Ibourt ",
+                    PersonType = Type.Student,
+                },
+                new Person
+                {
+                    Id = 4,
+                    FirstName = "Femke",
+                    LastName = "Vermander",
+                    PersonType = Type.Student,
+                },
+            };
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = Person.Scoops();
+
+            /*
+             * De meest voorkomende vorm is voor selectie en/of sorteren van C# objecten
+             * Dit zal dikwijls de DbSet van Entity Framework zijn
+             */
+            var vasteWerknemers = data.Where(x => x.PersonType == Person.Type.Owner || x.PersonType == Person.Type.Employee)
+                                      .OrderBy(x => x.FirstName); //IEnumerable<Person>
+
+
+            /*
+             * Soms heb je expliciet een kopie van de data nodig om te manipuleren
+             * ToList kopieerd de data in geheugen
+             * 
+             * Dit heeft wel een enorme Performance impact als je achterliggend type DbSet is
+             */
+            var vasteWerknemersList = data.Where(x => x.PersonType == Person.Type.Owner || x.PersonType == Person.Type.Employee).ToList(); //Generic.List<Person>
+
+            /*
+             * Een object zoeken volgens zijn Identiteit 
+             */
+            int id = 42;
+            var findPerson = data.Where(x => x.Id == id).FirstOrDefault();
+            if (findPerson == null)
+            {
+                throw new ArgumentException(); // Argument id is onzin
+            }
+
+            /*
+             * Belangrijk om te weten dat er ook een SQL geinspireerde syntax bestaat
+             * In combinatie met EF kan dit zeer performante queries opleveren
+             */
+            var nameList = from person in data
+                           where person.PersonType == Person.Type.Student
+                           orderby person.LastName
+                           select new { person.FirstName, person.LastName }; // Anonieme Type {string, string}
+
+        }
+    }
+}
